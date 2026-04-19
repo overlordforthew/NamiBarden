f084765b0629e470377948bacba613b6f6207192
Gil Barden <gil@namibarden.com>
2026-04-19 17:28:25 +0000

    Ai Chan: update course-catalog.js, entrypoint.sh, public/online-course-1.html, public/watch.html

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Ensure nginx can read all static files (guards against broken perms from docker cp)
+chmod -R a+rX /usr/share/nginx/html
+
 # Start nginx in background
 nginx -g 'daemon off;' &
 
